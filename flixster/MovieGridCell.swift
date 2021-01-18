2f53970ba1201cf861b372088df3d5d43a861348
Jingqi Yao <jessieyao0602@gmail.com>
2021-01-18 20:56:38 +0800

    Flixster Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jingqi Yao on 2021/1/18.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
